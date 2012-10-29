1dd4c78768416a0f0e6b4f0a7746eaf3c25a1e36
Kevin G. Fourie <kevin@4e.co.za>
2012-10-29 14:43:40 +0200

    Trying interfaces

--- examples/gofakeit.go
+++ examples/gofakeit.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
+	p := person.Person{}
+	p.Data.Populate("")
+
 	num := 10000
 	t0 := time.Now()
 	for i := 0; i < num; i++ {
-		fmt.Printf("%v\n", person.FullName())
+		fmt.Printf("%v\n", person.FullName(p))
 	}
 	t1 := time.Now()
 	fmt.Printf("\n%v Usernames generated and printed to stdout in %v (%v)\n", num, t1.Sub(t0), t1.Sub(t0).Seconds())
@@ -18,7 +21,7 @@ func main() {
 	num = 10000
 	t0 = time.Now()
 	for i := 0; i < num; i++ {
-		person.FullName()
+		person.FullName(p)
 	}
 	t1 = time.Now()
 	fmt.Printf("\n%v Usernames generated in %v (%v)\n", num, t1.Sub(t0), t1.Sub(t0).Seconds())
